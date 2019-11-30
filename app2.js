db8fd34d980de6e173f06cff994c841a4d1005ff
Will Smith <smithw1994@gmail.com>
2019-11-30 14:25:57 -0800

    Values of line display at mouse position

--- app2.js
+++ app2.js
@@ -12,6 +12,8 @@ const svg = d3.select("#chart").append("svg")
       .attr("transform",
       "translate(" + margin.left + "," + margin.top + ")")
 
+const test = d3.scaleOrdinal()
+
 const valueline = d3.line()
       .x(d => x(d.key))
       .y(d => y(d.value.playoffAvg))
@@ -37,7 +39,7 @@ dataset.then(data => {
   
   const parseTime = d3.timeParse("%Y");
   data.forEach(d => {
-    d.key = parseTime(d.key);
+    d.key = parseTime(d.key)
     d.value.playoffAvg = +d.value.playoffAvg
   });
   
@@ -70,6 +72,87 @@ dataset.then(data => {
         .attr("transform", "translate(" + -1*margin.left + "," + -1*margin.top/2 + ")")
         .style("text-anchor", "start")
         .text("Home Runs per Game");
+
+  const mouseG = svg.append("g")
+    .attr("class", "mouse-over-effects");
+
+  mouseG.append("path") // this is the black vertical line to follow mouse
+    .attr("class", "mouse-line")
+    .style("stroke", "black")
+    .style("stroke-width", "1px")
+    .style("opacity", "0");
+
+  const lines = document.getElementsByClassName('line');
+  
+  const mousePerLine = mouseG.selectAll(".mouse-per-line")
+    .data(data)
+    .enter()
+    .append("g")
+    .attr("class", "mouse-per-line");
+
+  mousePerLine.append("text")
+    .attr("transform", "translate(10,3)");
+
+  mouseG.append('svg:rect') // append a rect to catch mouse movements on canvas
+    .attr('width', width) // can't catch mouse events on a g element
+    .attr('height', height)
+    .attr('fill', 'none')
+    .attr('pointer-events', 'all')
+    .on('mouseout', function() { // on mouse out hide line, circles and text
+      d3.select(".mouse-line")
+        .style("opacity", "0");
+      d3.selectAll(".mouse-per-line circle")
+        .style("opacity", "0");
+      d3.selectAll(".mouse-per-line text")
+        .style("opacity", "0");
+    })
+    .on('mouseover', function() { // on mouse in show line, circles and text
+      d3.select(".mouse-line")
+        .style("opacity", "1");
+      d3.selectAll(".mouse-per-line circle")
+        .style("opacity", "1");
+      d3.selectAll(".mouse-per-line text")
+        .style("opacity", "1");
+    })
+    .on('mousemove', function() { // mouse moving over canvas
+      let mouse = d3.mouse(this);
+      d3.select(".mouse-line")
+        .attr("d", function() {
+          let d = "M" + mouse[0] + "," + height;
+          d += " " + mouse[0] + "," + 0;
+          return d;
+        });
+
+      d3.selectAll(".mouse-per-line")
+      .attr("transform", function(d, i) {
+        
+        let xDate = x.invert(mouse[0]),
+            bisect = d3.bisector(function(d) { return d.key; }).right;
+            idx = bisect(d.value, xDate);
+
+        if (i < 2) {
+          let beginning = 0,
+              end = lines[i].getTotalLength(),
+              target = null;
+            
+          while (true) {
+            target = Math.floor((beginning + end) / 2);
+            pos = lines[i].getPointAtLength(target);
+            if ((target === end || target === beginning) && pos.x !== mouse[0]) {
+                break;
+            }
+            if (pos.x > mouse[0])      end = target;
+            else if (pos.x < mouse[0]) beginning = target;
+            else break; //position found
+          }
+          
+          d3.select(this).select('text')
+            .text(y.invert(pos.y).toFixed(2));
+            
+          return "translate(" + mouse[0] + "," + pos.y +")";
+        }
+      });
+  });
 });
 
 
